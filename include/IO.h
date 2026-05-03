e944150335b8d421cb7a6b5006ac46042fc88d9b
Sergey Musikhin <musihin_sergei@mail.ru>
2026-05-03 23:27:36 +0300

    Move sockets operations to IO class

---
+++ include/IO.h
@@ -0,0 +1,86 @@
+#pragma once
+
+#include <cstdint>
+#include <vector>
+#include <span>
+#include <string_view>
+#include <iostream>
+
+#include <sys/socket.h>
+#include <arpa/inet.h> // for htons
+#include <net/ethernet.h>
+#include <net/if.h>
+#include <linux/if_packet.h>
+#include <unistd.h>
+#include <poll.h>
+
+struct InputPacket
+{
+    size_t inputIdx;
+    std::span<uint8_t> data;
+};
+
+class IO
+{
+public:
+    IO(const std::vector<std::string_view>& interfaces)
+    {
+        m_pollFds.reserve(interfaces.size());
+        for (const auto &interface: interfaces)
+        {
+            m_pollFds.push_back({createRawSocket(interface),POLLIN, 0});
+        }
+    }
+
+    std::vector<InputPacket> read()
+    {
+        int ret = poll(m_pollFds.data(), m_pollFds.size(), -1);
+        if (ret < 0) 
+            return {};
+
+        std::vector<InputPacket> result;
+        struct sockaddr_ll sll;
+        for (size_t i = 0; i < m_pollFds.size(); ++i) {
+            if (m_pollFds[i].revents & POLLIN) {
+                socklen_t sll_len = sizeof(sll);
+
+                ssize_t nread = recvfrom(m_pollFds[i].fd, m_buffer, sizeof(m_buffer), 0,
+                                         (struct sockaddr*)&sll, &sll_len);
+
+                if (nread <= 0) continue;
+
+                result.push_back({i, std::span<uint8_t>(m_buffer, nread)});
+            }
+        }
+
+        return result;
+    }
+
+    void write(const std::span<uint8_t>& data, size_t outputIndex)
+    {
+        ::write(m_pollFds[outputIndex].fd, data.data(), data.size());
+    }
+
+private:
+    int createRawSocket(std::string_view interface)
+    {
+        int sock = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
+        if (sock < 0)
+        {
+            std::cout << "socket error\n";
+        }
+
+        struct sockaddr_ll sll;
+        sll.sll_family = AF_PACKET;
+        sll.sll_ifindex = if_nametoindex(interface.data());
+        sll.sll_protocol = htons(ETH_P_ALL);
+
+        (void)bind(sock, (struct sockaddr *)&sll, sizeof(sll));
+
+        return sock;
+    }
+
+private:
+    uint8_t m_buffer[2048];
+    std::vector<pollfd> m_pollFds;
+};
--- main.cpp
+++ main.cpp
@@ -1,17 +1,9 @@
 #include "EthHeaderView.h"
 #include "MACView.h"
-
-#include <sys/socket.h>
-#include <arpa/inet.h> // for htons
-#include <net/ethernet.h>
-#include <net/if.h>
-#include <linux/if_packet.h>
-#include <unistd.h>
+#include "IO.h"
 
 #include <iostream>
 #include <vector>
-
-#include <poll.h>
 
 struct ForwardDecision
 {
@@ -19,9 +11,9 @@ struct ForwardDecision
     std::vector<size_t> outPorts;
 };
 
-ForwardDecision ingress(uint8_t* buffer, size_t nread, size_t inPort, size_t portsCount)
+ForwardDecision ingress(InputPacket inputPacket, size_t portsCount)
 {
-    EthHeaderView eth(buffer, nread);
+    EthHeaderView eth(inputPacket.data.data(), inputPacket.data.size());
 
     if (!eth.valid())
     {
@@ -37,89 +29,50 @@ ForwardDecision ingress(uint8_t* buffer, size_t nread, size_t inPort, size_t por
     ForwardDecision fd{};
     for (size_t j = 0; j < portsCount; ++j)
     {
-        if (j != inPort)
+        if (j != inputPacket.inputIdx)
             fd.outPorts.push_back(j);
     }
 
     return fd;
 }
 
-void egress(const ForwardDecision& fd,
-            uint8_t* buffer,
-            size_t nread,
-            const std::vector<pollfd>& pollFds)
+void egress(
+    IO& io,
+    const ForwardDecision& fd,
+    const std::span<uint8_t>& buffer)
 {
     if (fd.drop) return;
 
     for (const auto& outPort : fd.outPorts) {
         std::cout << "Write to " << outPort << '\n';
-        write(pollFds[outPort].fd, buffer, nread);
+        io.write(buffer, outPort);
     }
 }
-
-void runHub(const std::vector<int>& fds) {
-    std::vector<pollfd> poll_fds;
-    for (int fd : fds) {
-        poll_fds.push_back({fd, POLLIN, 0});
-    }
-
-    unsigned char buffer[2048];
-    struct sockaddr_ll sll;
-    while (true) {
-        int ret = poll(poll_fds.data(), poll_fds.size(), -1);
-        if (ret < 0) break;
-
-        for (size_t i = 0; i < poll_fds.size(); ++i) {
-            if (poll_fds[i].revents & POLLIN) {
-                socklen_t sll_len = sizeof(sll);
-
-                ssize_t nread = recvfrom(poll_fds[i].fd, buffer, sizeof(buffer), 0,
-                                         (struct sockaddr*)&sll, &sll_len);
-
-                if (nread <= 0) continue;
-
-                std::cout << "Received " << nread << " bytes by " << i << '\n';
-
-                if (sll.sll_pkttype == PACKET_OUTGOING) continue;
-
-                auto decision = ingress(buffer, nread, i, poll_fds.size());
-                egress(decision, buffer, nread, poll_fds);
-
-            }
-        }
-    }
-}
-
-int createRawSocket(const char* interfaceName)
-{
-    int sock = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
-    if (sock < 0)
-    {
-        std::cout << "socket error\n";
-    }
-
-    struct sockaddr_ll sll;
-    sll.sll_family = AF_PACKET;
-    sll.sll_ifindex = if_nametoindex(interfaceName);
-    sll.sll_protocol = htons(ETH_P_ALL);
-
-    (void)bind(sock, (struct sockaddr *)&sll, sizeof(sll));
-
-    return sock;
-
-}
-
 
 int main( int argc, char** argv)
 {
-    std::vector<int> sockets;
-    sockets.reserve(argc - 1);
+    std::vector<std::string_view> interfaces;
+    interfaces.reserve(argc - 1);
     for (int i = 1; i < argc; ++i)
     {
-        sockets.push_back(createRawSocket(argv[i]));
-        std:: cout << argv[i] << '\n';
+        interfaces.push_back(argv[i]);
     }
 
-    runHub(sockets);
+    IO io(interfaces);
+
+    while (true)
+    {
+        auto packets = io.read();
+        if(packets.empty())
+        {
+            return 0;
+        }
+        for (const auto & packet: packets)
+        {
+            auto decision = ingress(packet, interfaces.size());
+            egress(io, decision, packet.data);
+        }
+    }
+
     return 0;
 }
