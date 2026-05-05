03e16319727e0568b0f7b00882ba1df0a3fad6ba
Sergey Musikhin <musihin_sergei@mail.ru>
2026-05-06 00:09:19 +0300

    Move work to NetworkSwitch

---
+++ include/NetworkSwitch.h
@@ -0,0 +1,83 @@
+#pragma once
+
+#include "IO.h"
+
+#include "EthHeaderView.h"
+#include "MACView.h"
+
+class NetworkSwitch
+{
+public:
+    NetworkSwitch(IO& io, size_t portsCount) : m_io{io}, m_portsCount{portsCount}
+    {
+    }
+
+    void run()
+    {
+        while (true)
+        {
+            auto packets = m_io.read();
+            if(packets.empty())
+            {
+                return;
+            }
+
+            for (const auto & packet: packets)
+            {
+                auto decision = ingress(packet);
+                egress(m_io, decision, packet.data);
+            }
+        }
+    }
+
+    struct ForwardDecision
+    {
+        bool drop = false;
+        std::vector<size_t> outPorts;
+    };
+
+private:
+    ForwardDecision ingress(InputPacket inputPacket)
+    {
+        EthHeaderView eth(inputPacket.data.data(), inputPacket.data.size());
+
+        if (!eth.valid())
+        {
+            return {.drop = true};
+        }
+
+        std::cout << "Ethernet type: 0x"
+                << std::hex << eth.ethertype() << std::dec << '\n';
+
+        std::cout << "Destination: " << MACView(eth.dst()) << '\n';
+        std::cout << "Source: " << MACView(eth.src()) << '\n';
+
+        ForwardDecision fd{};
+        for (size_t j = 0; j < m_portsCount; ++j)
+        {
+            if (j != inputPacket.inputIdx)
+                fd.outPorts.push_back(j);
+        }
+
+        return fd;
+    }
+
+    void egress(
+        IO& io,
+        const ForwardDecision& fd,
+        const std::span<uint8_t>& buffer)
+    {
+        if (fd.drop) return;
+
+        for (const auto& outPort : fd.outPorts) {
+            std::cout << "Write to " << outPort << '\n';
+            io.write(buffer, outPort);
+        }
+    }
+
+
+
+ private:
+    IO& m_io;
+    const size_t m_portsCount;
+ };
--- main.cpp
+++ main.cpp
@@ -1,53 +1,9 @@
-#include "EthHeaderView.h"
-#include "MACView.h"
 #include "IO.h"
+#include "NetworkSwitch.h"
 
 #include <iostream>
 #include <vector>
 
-struct ForwardDecision
-{
-    bool drop = false;
-    std::vector<size_t> outPorts;
-};
-
-ForwardDecision ingress(InputPacket inputPacket, size_t portsCount)
-{
-    EthHeaderView eth(inputPacket.data.data(), inputPacket.data.size());
-
-    if (!eth.valid())
-    {
-        return {.drop = true};
-    }
-
-    std::cout << "Ethernet type: 0x"
-              << std::hex << eth.ethertype() << std::dec << '\n';
-
-    std::cout << "Destination: " << MACView(eth.dst()) << '\n';
-    std::cout << "Source: " << MACView(eth.src()) << '\n';
-
-    ForwardDecision fd{};
-    for (size_t j = 0; j < portsCount; ++j)
-    {
-        if (j != inputPacket.inputIdx)
-            fd.outPorts.push_back(j);
-    }
-
-    return fd;
-}
-
-void egress(
-    IO& io,
-    const ForwardDecision& fd,
-    const std::span<uint8_t>& buffer)
-{
-    if (fd.drop) return;
-
-    for (const auto& outPort : fd.outPorts) {
-        std::cout << "Write to " << outPort << '\n';
-        io.write(buffer, outPort);
-    }
-}
 
 int main( int argc, char** argv)
 {
@@ -59,20 +15,8 @@ int main( int argc, char** argv)
     }
 
     IO io(interfaces);
-
-    while (true)
-    {
-        auto packets = io.read();
-        if(packets.empty())
-        {
-            return 0;
-        }
-        for (const auto & packet: packets)
-        {
-            auto decision = ingress(packet, interfaces.size());
-            egress(io, decision, packet.data);
-        }
-    }
+    NetworkSwitch networkSwitch(io, interfaces.size());
+    networkSwitch.run();
 
     return 0;
 }
